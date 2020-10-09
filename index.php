ae5e567689bf7963167af262a8c027e24b133cf3
JF Padilla <emailreel@gmail.com>
2020-10-08 21:22:09 -0400

    html: construct initial web interface
    Develop the initial website markup to be used for the application.

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Brute MD5 Inverter</title>
   </head>
   <body>
+    <h1>MD5 Hash Inverter</h1>
 
+    <h2>About</h2>
+    <p>This web document provides a tool to invert an MD5 hash of a string
+    consisting of a one-digit integer. The inverter employs a brute-force
+    search to determine the string associated with the MD5 hash.</p>
+
+    <h2>Inverter</h2>
+    <form>
+      <input type="text" name="md5hash"
+        placeholder="Enter MD5 hash here."
+        pattern="[0-9a-f]{32}" title="a 32 digit hexadecimal value"
+        maxlength="32" size="48" required autofocus>
+      <br><br>
+      <input type="reset" value="clear field">
+    </form>
   </body>
 </html>
